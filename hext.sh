31f0f29c74447c6c9810759d71e8473dcaa327bc
laib3 <54323877+laib3@users.noreply.github.com>
2021-11-16 11:47:14 +0100

    feat: add hext

---
+++ hext.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# hext: a simple hexadecimal color extractor 
+# prints out the 16 main colors from a picture
+
+HELP="usage: hext <picture.jpg>"
+
+[ $# -ne 1 ] && echo "${HELP}" && exit 1
+[ ! -f "${1}" ] && echo "error: ${1} does not exist" && exit 2
+
+convert "${1}" +dither -colors 16 -unique-colors -format "%c" -depth 8 txt:- | \
+	sed '1d; s/.*\(#.\{6\}\).*/\1/'
